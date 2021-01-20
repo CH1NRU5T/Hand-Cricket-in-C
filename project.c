1aec1388aa9977161f2f391b73ec9995968d3b2d
CH1NRU5T <chinrust@gmail.com>
2021-01-20 20:29:31 +0530

    added instructions

--- project.c
+++ project.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+void instructions();
 int generateRandoms(int, int);
 int execute(int entered, int sum);
 void printEnd(int);
 int main()
 {
+    instructions();
     int lower = 1, upper = 6, num, sum = 0, result;
     srand(time(0));
     do
@@ -16,6 +18,11 @@ int main()
         sum = result;
     } while (result != 0);
     return 0;
+}
+void instructions()
+{
+    printf("Hello! Welcome to the 'Hand Cricket' game. Refreshing some of your childhood memories eh...\n\n");
+    printf("The rules are simple! You have to score runs against the computer. Just type the number of runs you want to score, if the number you entered matches the number thrown by the computer, YOU'RE OUT!. If it does not match, carry on and build up your score.\n\n\n");
 }
 int generateRandoms(int lower, int upper)
 {
